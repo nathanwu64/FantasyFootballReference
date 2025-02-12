abdabee56104710d6d12889401d55d57ba45e189
Nathan Wu <nathanwu@Nathans-MacBook-Air.local>
2025-02-11 21:27:09 -0800

    h2h cards

--- sleeper_project/views/manager.py
+++ sleeper_project/views/manager.py
@@ -5,17 +5,17 @@ import json
 from sleeper_project.views.models.manager import Manager
 
 def manager_page(request, username):
-    # m = Manager(username)
-    # m.get_manager_page_data()
+    m = Manager(username)
+    m.get_manager_page_data()
     return render(request, 'manager.html',{
-        # 'username': username,
-        # 'league_wins': m.leagues_won,
-        # 'toilet_wins': m.toilets_won,
-        # 'consolation_wins': m.consolations_won,
-        # 'manager_since': m.manager_since,
-        # 'total_leagues': m.total_leagues,
-        # 'championships': m.championships,
-        # 'records': m.records,
-        # 'top_players': m.top_players
+        'username': username,
+        'league_wins': m.leagues_won,
+        'toilet_wins': m.toilets_won,
+        'consolation_wins': m.consolations_won,
+        'manager_since': m.manager_since,
+        'total_leagues': m.total_leagues,
+        'championships': m.championships,
+        'records': m.records,
+        'top_players': m.top_players
     })
 
